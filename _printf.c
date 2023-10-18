c8bd62b2d96c81e5627b75cd8f338303e763c525
HucksApp <hucksapp@gmail.com>
2023-10-18 13:44:38 -0700

    new

--- _printf.c
+++ _printf.c
@@ -1,96 +1,77 @@
 #include "main.h"
 
-
-
 /**
- * _find_fmtf - Print Format finder
- *@fmspr: format specifier
- * Return: NULL or Format Function
-*/
-
-int (* _find_fmtf(const char *fmspr))(va_list)
+  * find_function - function that finds formats for _printf
+  * calls the corresponding function.
+  * @format: format (char, string, int, decimal)
+  * Return: NULL or function associated ;
+  */
+int (*find_function(const char *format))(va_list)
 {
-	unsigned int index = 0;
-
-	prt_fmt prt_fmts[] = {
-
-	{"c", _print_char},
-	{"i", _print_int},
-	{"s", _print_string},
-	{"d", _print_int},
-	{"b", _print_bin},
-	{"u", _print_unsig},
-	{"R", _print_rot13},
-	{"o", _print_oct},
-	{"r", _print_rev},
-	{"x", _print_hexx},
-	{"X", _print_hexX},
-	{NULL, NULL}
+	unsigned int i = 0;
+	prt_fmt find_f[] = {
+		{"c", _print_char},
+		{"s", _print_string},
+		{"i", _print_int},
+		{"d", _print_int},
+		{"r", _print_rev},
+		{"b", _print_bin},
+		{"u", _print_unsig},
+		{"o", _print_oct},
+		{"x", _print_hexx},
+		{"X", _print_hexX},
+		{"R", _print_rot13},
+		{NULL, NULL}
 	};
-	while (prt_fmts[index].spc)
+
+	while (find_f[i].spc)
 	{
-		if (*(prt_fmts[index].spc) == *fmspr)
-		return (prt_fmts[index].fn);
-		index++;
+		if (find_f[i].spc[0] == (*format))
+			return (find_f[i].fn);
+		i++;
 	}
 	return (NULL);
-
 }
-
-
-
-
-
 /**
- * _printf - Print Format finder
- * @str: Formated String
- *@...: variables to be inserted into the stream
- * Return: NULL or Format Function
-*/
-int _printf(const char *str, ...)
+  * _printf - function that produces output according to a format.
+  * @format: format (char, string, int, decimal)
+  * Return: size the output text;
+  */
+int _printf(const char *format, ...)
 {
 	va_list ap;
-	unsigned int print_count = 0, char_count = 0;
-	int (*fmt)(va_list);
+	int (*f)(va_list);
+	unsigned int i = 0, cprint = 0;
 
-	if (!str || (str[0] == FMT_SYB && str[1] == '\0'))
-		return (0);
-	va_start(ap, str);
-	while (str[char_count])
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[i])
 	{
-		switch (str[char_count])
+		while (format[i] != '%' && format[i])
 		{
-		case ('\0'):
-			return (print_count);
-		case (FMT_SYB):
-			if (!str[char_count + 1])
-				return (1);
-			fmt = _find_fmtf(&(str[char_count + 1]));
-			if (fmt != NULL)
-			{
-				print_count += fmt(ap);
-				char_count += 2;
-			}
-			else if (fmt == NULL)
-			{
-				if (str[char_count] == FMT_SYB &&
-				    str[char_count + 1] == ' ' && str[char_count + 2] != FMT_SYB)
-					return (char_count);
-				_putchar(str[char_count]);
-				print_count++;
-				char_count++;
-				if (str[char_count] == FMT_SYB)
-					char_count++;
-			}
-
-			break;
-		default:
-			_putchar(str[char_count]);
-			print_count++;
-			char_count++;
-			break;
+			_putchar(format[i]);
+			cprint++;
+			i++;
 		}
+		if (format[i] == '\0')
+			return (cprint);
+		f = find_function(&format[i + 1]);
+		if (f != NULL)
+		{
+			cprint += f(ap);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		cprint++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
 	va_end(ap);
-	return (print_count);
+	return (cprint);
 }
---
+++ main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    	_printf("%S\n", "Best\nSchool");
+	_printf("%b\n", 98);
+	return (0);
+}
